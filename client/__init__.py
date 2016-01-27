3d88dfb392228b8bf7c9a394395368829169e19d
Krukov <glebov.ru@gmail.com>
2016-01-27 19:00:07 +0300

    Quote url

--- client/__init__.py
+++ client/__init__.py
@@ -15,12 +15,12 @@ from socket import gaierror
 
 try:
     from httplib import HTTPConnection, HTTPSConnection  # Py<3
-    from urllib import urlencode
+    from urllib import urlencode, quote
     from socket import error as ConnectionError
     input = raw_input
 except ImportError:
     from http.client import HTTPConnection, HTTPSConnection  # Py>=3
-    from urllib.parse import urlencode
+    from urllib.parse import urlencode, quote
 
 
 _DEBUG = False
@@ -367,6 +367,10 @@ def _is_debug():
     return '--debug' in sys.argv
 
 
+def _format_alias(alias):
+    return quote(alias, safe='')
+
+
 def get_options_parser():
     """Arguments definition"""
     parser = argparse.ArgumentParser(description='Tool for creating notes', prog='noteit')
@@ -424,16 +428,16 @@ def main():
             alias = options.alias
             if options.overwrite:
                 try:
-                    delete_note(alias)
+                    delete_note(_format_alias(alias))
                 except:
                     pass
             display(create_note(note, alias))
 
         elif options.alias:
             if options.delete:
-                display(delete_note(options.alias))
+                display(delete_note(_format_alias(options.alias)))
             else:
-                display(get_note(options.alias))
+                display(get_note(_format_alias(options.alias)))
         elif options.last:
             display(get_last_note())
         elif not options.note:
