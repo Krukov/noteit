9472594303a33cff0ccc75381d1b7312d2c12dbf
Krukov <glebov.ru@gmail.com>
2016-02-02 08:31:59 +0300

    Fix for -u option

--- client/__init__.py
+++ client/__init__.py
@@ -334,7 +334,8 @@ def _get_key():
             return key
         with open(key) as key_file:
             return key_file.read()
-    if os.path.isfile(_KEY_PATH):
+
+    if not get_options().user and os.path.isfile(_KEY_PATH):
         with open(_KEY_PATH) as key_file:
             return key_file.read()
     return _get_secret_hash()
@@ -352,8 +353,6 @@ def _get_secret_hash():
 
 
 def _save_key():
-    if os.path.isfile(_KEY_PATH):
-        return
     password = _get_secret_hash()
     _save_file_or_ignore(_KEY_PATH, password)
 
@@ -608,7 +607,7 @@ def main():
             sys.exit('Something went wrong! You can sent report to us with "-r" option')
         report(traceback.format_exc())
 
-    if not options.do_not_save and not _get_token_from_system() and not options.drop_tokens:
+    if options.user or (not options.do_not_save and not _get_token_from_system() and not options.drop_tokens):
         token = _get_token()
         if token:
             _save_token(token)
